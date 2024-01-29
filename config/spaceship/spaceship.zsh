279ffcdf23d50637fef54e6a02f87fe36a78d34a
Konstantinos Samaras-Tsakiris <konstantinos.samaras-tsakiris@neuralconcept.com>
2024-01-29 16:21:18 +0100

    Remove noisy spaceship sections

--- config/spaceship/spaceship.zsh
+++ config/spaceship/spaceship.zsh
@@ -3,3 +3,10 @@ SPACESHIP_CHAR_SUFFIX=" " # space before the prompt
 SPACESHIP_EXEC_TIME_PREFIX="" # show only the time with no text
 SPACESHIP_EXEC_TIME_ELAPSED=10 # show exec time when above 10s
 SPACESHIP_BATTERY_THRESHOLD=20 # show battery percentage when below 20%
+SPACESHIP_DIR_TRUNC=5
+
+SPACESHIP_DOCKER_SHOW=false
+SPACESHIP_DOCKER_COMPOSE_SHOW=false
+SPACESHIP_GCLOUD_SHOW=false
+SPACESHIP_AWS_SHOW=false
+SPACESHIP_AZURE_SHOW=false
